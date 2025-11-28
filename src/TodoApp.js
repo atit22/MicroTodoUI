4b97a0a1d20f8c6296331af67d799bc9b01a7730
atit22 <111438927+atit22@users.noreply.github.com>
2025-11-28 23:46:42 +0530

    Update TodoApp.js

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -6,9 +6,9 @@ import { Box } from '@mui/material';
 import config from './config'; // Import the config file with environment variable support
 
 // Use the config to define API URLs
-const GET_TASKS_API_BASE_URL = "http://dev-get-tasks-api.jhandu.shop";
-const DELETE_TASK_API_BASE_URL = "http://dev-delete-task-api.jhandu.shop";
-const CREATE_TASK_API_BASE_URL = "http://dev-add-task-api.jhandu.shop";
+const GET_TASKS_API_BASE_URL = "http://10.0.119.127";
+const DELETE_TASK_API_BASE_URL = "http://10.0.241.203";
+const CREATE_TASK_API_BASE_URL = "http://10.0.175.153";
 
 //Update Task Functionality is Work In Progress
 // const UPDATE_TASK_API_BASE_URL = '';
